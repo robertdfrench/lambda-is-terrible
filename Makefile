d53054bfc335e5faa22a2d8556a1a03be8050b59
Robert D. French <robert@robertdfrench.me>
2020-02-17 12:02:22 -0500

    Make wsgi line up with cgi benchmark

--- Makefile
+++ Makefile
@@ -7,28 +7,34 @@ slides.pdf: slides.tex
 	texi2pdf --batch --clean $<
 
 cgi_benchmark: clean
-ifndef ITERATIONS
-	$(error "set $$ITERATIONS=1024, to simulate 1024 requests")
+ifndef REQUESTS
+	$(error "set $$REQUESTS=1024, to simulate 1024 requests")
 endif
-	time -p ./cgi_benchmark_wrapper.sh $(ITERATIONS) > /dev/null
+	time -p ./cgi_benchmark_wrapper.sh $(REQUESTS) > /dev/null
 
 cgi_benchmark_concurrent:
-ifndef NUM_TASKS
-	$(error "set $$NUM_TASKS=2, for 2 cpus")
+ifndef CONCURRENCY
+	$(error "set $$CONCURRENCY=2, for 2 cpus")
 endif
-ifndef ITERATIONS
-	$(error "set $$ITERATIONS=1024, to simulate 1024 requests")
+ifndef REQUESTS
+	$(error "set $$REQUESTS=1024, to simulate 1024 requests")
 endif
 	@echo -n "Requests per second: "
-	@for i in `seq 1 $(NUM_TASKS)`; do ($(MAKE) cgi_benchmark ITERATIONS=$(ITERATIONS) &); done 2>&1 \
+	@for i in `seq 1 $(CONCURRENCY)`; do ($(MAKE) cgi_benchmark REQUESTS=$(REQUESTS) &); done 2>&1 \
 		| awk '/real/ { print $$2 }' \
 		| sort -g \
 		| tail -n1 \
-		| sed 's,^,$(ITERATIONS)*$(NUM_TASKS)/,' \
+		| sed 's,^,$(REQUESTS)*$(CONCURRENCY)/,' \
 		| bc
 
 wsgi_benchmark:
-	ab -m GET "http://127.0.0.1:8000/"
+ifndef CONCURRENCY
+	$(error "set $$CONCURRENCY=2, for 2 cpus")
+endif
+ifndef REQUESTS
+	$(error "set $$REQUESTS=1024, to simulate 1024 requests")
+endif
+	ab -c $(CONCURRENCY) -n $(REQUESTS) -m GET "http://127.0.0.1:8000/"
 
 clean:
 	rm -rf \
--- wsgi.py
+++ wsgi.py
@@ -2,26 +2,36 @@
 import sqlite3
 from wsgiref.simple_server import make_server
 
-conn = sqlite3.connect("hit_counter.sqlite3")
+def connect_to_db():
+    dbh = sqlite3.connect("hit_counter.sqlite3")
+    dbh.execute("CREATE TABLE IF NOT EXISTS counters(hits INTEGER, ip TEXT PRIMARY KEY)")
+    return dbh
+
+def generate_summary_of_visits(dbh):
+    result = dbh.execute("SELECT SUM(hits), COUNT(*) from counters")
+    (visits, visitors) = result.fetchone()
+    return f"This page has been viewed {visits} times by {visitors} unique visitors"
+
+def record_new_visit(dbh, ip):
+    with dbh:
+        dbh.execute("""
+            INSERT INTO counters(hits,ip) VALUES(1,?)
+            ON CONFLICT(ip) DO
+            UPDATE SET hits = (counters.hits + 1) WHERE ip=?
+            """, (ip, ip))
 
 def hit_counter(environ, start_response):
+    ip = '0.0.0.0'
+    record_new_visit(dbh, ip)
+    summary = generate_summary_of_visits(dbh)
+
     status = '200 OK'
     headers = [('Content-type', 'text/plain; charset=utf-8')]
     start_response(status, headers)
+    return [summary.encode('utf-8')]
 
-    ip = '0.0.0.0'
-    with conn:
-        conn.execute("""
-        INSERT INTO counters(hits,ip) VALUES(1,?)
-        ON CONFLICT(ip) DO
-        UPDATE SET hits = (counters.hits + 1) WHERE ip=?
-        """, (ip, ip))
-    result = conn.execute("SELECT SUM(hits), COUNT(*) from counters")
-    (visits, visitors) = result.fetchone()
-    response = f"This page has been viewed {visits} times by {visitors} unique visitors"
-    return [response.encode('utf-8')]
-
-conn.execute("CREATE TABLE IF NOT EXISTS counters(hits INTEGER, ip TEXT PRIMARY KEY)")
+# Init
+dbh = connect_to_db()
 with make_server('', 8000, hit_counter) as httpd:
     print("Serving on port 8000...")
     httpd.serve_forever()
